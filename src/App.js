6b99e7d26647cffdd86ae0be7a095e3cc8f3e7e3
*Collins <nyamburycollins@mail.com>
2025-04-26 06:11:36 +0300

    Assignment completed

--- src/App.js
+++ src/App.js
@@ -1,46 +1,60 @@
-import React, { Component } from 'react';  // Fixed: Import `Component`
-import Timer from './Timer';
-
-class App extends Component {
-  state = {
-    timerIDs: []
-  };
-
-  // Optional: Add a timer on mount (if intentional)
-  componentDidMount() {
-    // this.handleAddTimer(); // Uncomment if you want a timer on load
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>MultiTimer</h1>
-        <button onClick={this.handleAddTimer}>Add New Timer</button>
-        <div className="TimerGrid">
-          {this.renderTimers()}
-        </div>
-      </div>
-    );
-  }
-
-  renderTimers = () => {
-    return this.state.timerIDs.map(id => (
-      <Timer key={id} id={id} removeTimer={this.removeTimer} />
-    ));
-  };
-
-  // Generate a more unique ID (using timestamp)
-  handleAddTimer = () => {
-    this.setState(prevState => ({
-      timerIDs: [...prevState.timerIDs, Date.now()]
-    }));
-  };
-
-  removeTimer = id => {
-    this.setState(prevState => ({
-      timerIDs: prevState.timerIDs.filter(timer_id => timer_id !== id)
-    }));
-  };
-}
-
-export default App;
+import React from 'react';
+ import Timer from './Timer'
+ 
+ class App extends React.Component {
+ 
+   //no props being used here, so we can use the shorthand declaration of state
+   state = {
+     timerIDs: []
+   }
+ 
+   //Your code here:
+   componentDidMount() {
+     fetch(`http://localhost:3000/timers`)
+     .then(r => r.json())
+     .then(timer => {
+       this.setState([...this.state.timerIDs, timer])
+     })
+     this.handleAddTimer()
+   }
+ 
+   // No need to modify anything in render or the class methods below
+   // Unless, of course, you're curious about how it all works
+   render() {
+ 
+     return (
+       <div className="App">
+         <h1>MultiTimer</h1>
+         <button onClick={this.handleAddTimer}>Add New Timer</button>
+ 
+         <div className="TimerGrid">
+           {this.renderTimers()}
+         </div>
+ 
+       </div>
+     );
+   }
+ 
+   // returns array of components written in JSX, mapped from this.state.timerIDs
+   renderTimers = () => this.state.timerIDs.map(id => {
+     return <Timer key={id} id={id} removeTimer={this.removeTimer} />
+   })
+ 
+   // adds a random number for timer ID
+   handleAddTimer = () => {
+     this.setState(prevState => ({
+       timerIDs: [...prevState.timerIDs, Math.floor(Math.random()*1000)]
+     }))
+   }
+ 
+   // removeTimer updates state, removing any timer that matches the provided author
+   removeTimer = id => {
+     this.setState(prevState => ({
+       timerIDs: prevState.timerIDs.filter(timer_id => timer_id !== id)
+     }))
+   }
+ 
+ 
+ }
+ 
+ export default App;
